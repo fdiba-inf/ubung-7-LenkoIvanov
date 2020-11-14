5adc770a176a7ecde870b26a92773101603a282b
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-11-14 13:22:22 +0000

    ubung7

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -9,15 +9,37 @@ public class RecursivePrimePrinter {
         System.out.print("Enter n: ");
         int n = input.nextInt();
 
+       
         printPrimes(n);
+       
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
+      if(n == 1){
+        System.out.println(n);
+        return;
+      }
+
+      if(isPrime(n)){
+        System.out.println(n);
+      }
+
+      printPrimes(n - 1);
+
     }
 
+
     public static boolean isPrime(int n) {
-        // Implement method
-        return false;
+    boolean prime = true;
+    int divider = 2;
+    int maxDivider = (int) Math.sqrt(n);
+    
+    while(prime && divider <= maxDivider){
+      if(n % divider == 0){
+        prime = false;
+      }
+      divider++;
+    }
+    return prime;
     }
 }
